3d637964b9ae18caced18be18031d7d198ebabc5
Andre1Karl <ramos.andreikarl.gmail.com>
2022-06-17 11:25:10 +0800

    update(2)

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var t=0;
 
-function addtocart(name,price){
+function addtocart(id,name,price){
+	var testData = !!document.getElementById(id);
+	if (testData) {
+		add(id,price);
+	} else{
 	const order = document.createElement("tr");
-	order.innerHTML = '<td>' +name+ '</td><td>1</td><td>' +price+ '</td>';
+	order.setAttribute('id',id);
+	order.innerHTML = '<td>' +name+ '</td><td><button onclick="minus(\''+id+'\','+parseFloat(price)+')">-</button><span>1</span><button onclick="add(\''+id+'\','+price+')">+</button></td><td>' +parseFloat(price)+ '</td>';
 	document.getElementById('items').appendChild(order);
 	t=t+parseFloat(price);
-	document.querySelector('#total').innerHTML = 'TO BE PAID:' + t.toFixed(2);
+	document.querySelector('#total').innerHTML = 'TO BE PAID: ' + t.toFixed(2);
+	document.querySelector('#price').innerHTML = 'YOU ARE PAYING: ' + t.toFixed(2);
+}
+}
+function add(id,price){
+	var current = parseInt(document.querySelector('#'+id+' span').innerHTML);
+	document.querySelector('#' +id+' span').innerHTML=current+1;
+	t=t+parseFloat(price);
+	document.querySelector('#total').innerHTML = 'TO BE PAID: ' + parseFloat(t).toFixed(2);
+	document.querySelector('#price').innerHTML = 'YOU ARE PAYING: ' + t.toFixed(2);
+}
+function minus(id,price){
+	var current = parseInt(document.querySelector('#'+id+' span').innerHTML);
+	document.querySelector('#'+id+' span').innerHTML=current-1;
+	t=t-parseFloat(price);
+	document.querySelector('#total').innerHTML = 'TO BE PAID: ' + parseFloat(t).toFixed(2);
+	document.querySelector('#price').innerHTML = 'YOU ARE PAYING: ' + t.toFixed(2);
+	if (current-1<=0){
+		document.querySelector('#'+ id).remove();
+	}
 }
